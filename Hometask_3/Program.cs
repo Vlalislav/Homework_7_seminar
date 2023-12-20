330ccbf7a21fd8d6e3c8d1ffd8b86ac68da4b572
Vladislav Kupreychuk <vla19986@gmail.com>
2023-12-20 21:20:09 +0300

    b

---
+++ Hometask_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы
+
+
+
+
+
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        PrintArrayReversed(array, array.Length - 1);
+    }
+
+    static void PrintArrayReversed(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(array[index]+" ");
+            PrintArrayReversed(array, index - 1);
+        }
+    }
+
